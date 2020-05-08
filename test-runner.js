5c47489de9ecbf33c39a13ee8e59018c7ed33478
void00 <tideback@gmail.com>
2020-05-08 18:03:32 +0200

    test-runner

---
+++ test-runner.js
@@ -0,0 +1,17 @@
+// run with npm test
+// or npm --feature=featureFileName test
+// for only running specific feature
+
+let whatFeature = process.env.npm_config_feature;
+if (whatFeature && !whatFeature.includes('.feature')) {
+  whatFeature += '.feature';
+}
+
+let toRun = `./node_modules/selenium-cucumber-js/index.js`
+
+process.argv.push('-b', 'firefox')
+process.argv.push('-x', '40000')
+if (whatFeature) {
+  process.argv.push('-f', 'features/' + whatFeature);
+}
+require(toRun);
