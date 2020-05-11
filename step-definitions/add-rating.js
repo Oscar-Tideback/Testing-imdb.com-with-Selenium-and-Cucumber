7067d6bb4efae476616af20d128c88e1470ca7a3
void00 <tideback@gmail.com>
2020-05-11 16:54:06 +0200

    Some step-def

---
+++ step-definitions/add-rating.js
@@ -0,0 +1,20 @@
+let { $, sleep } = require('./funcs');
+
+module.exports = function () {
+
+  //Scenario: When I have signed in I would lik to add rating on objects
+  this.Given(/^that I have signed in to the page$/, function () {
+  });
+  this.Then(/^click the first move in "([^"]*)"$/, function (headingMovies2Watch) {
+  });
+  this.Then(/^get name of movie$/, function (callback) {
+  });
+  this.Then(/^click (\d+) rating for that move$/, function (rating7) {
+  });
+  this.Then(/^then the rating should be added to my rating page$/, function () {
+  });
+  this.Then(/^check name of first move in list$/, function () {
+  });
+  this.Then(/^that move should have (\d+) as a rating$/, function (rating7) {
+  });
+}
---
+++ step-definitions/check-birthday.js
@@ -0,0 +1,19 @@
+let { $, sleep } = require('./funcs');
+
+module.exports = function () {
+
+  //Scenario: Cross-check our date of birth with the celebertys everipedia pages
+  this.When(/^I browse to Birth Month Day of todays$/, function () {
+  });
+  this.Then(/^the first (\d+) names should be saved to a list$/, function (value5) {
+  });
+  this.Then(/^if the list contains white space replace that with _$/, function () {
+  });
+  this.Then(/^add "([^"]*)" before the name$/, function (everipediaUrl) {
+  });
+  this.Then(/^browse to that page$/, function () {
+  });
+  this.Then(/^find birthday on that page and check if it is today$/, function () {
+  });
+
+}
