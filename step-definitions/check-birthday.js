2662c2fbe7f3848d6a2718a1ecfdd25a910e0b31
void00 <tideback@gmail.com>
2020-05-12 16:08:09 +0200

    some message in bday step-def

--- step-definitions/check-birthday.js
+++ step-definitions/check-birthday.js
@@ -40,7 +40,7 @@ module.exports = function () {
       let str = await wikiBday[i].getAttribute("textContent");
       await sleep(1000);
       let str2 = str.slice(5, str.length);
-      if (today.getMonth() < 9)//This might be wrong
+      if (today.getMonth() < 9)//This might be wrong, should it be 10?
         rightDate = str2.slice(1, str2.length);
       expect(ourDate,
         'Our date for this celebrity should be'
