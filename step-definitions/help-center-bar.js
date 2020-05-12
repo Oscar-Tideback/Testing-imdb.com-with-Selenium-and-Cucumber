40fb5e56930f07ca53a2731d2bf73205f46ebede
FireRay <jonashoglund282@gmail.com>
2020-05-12 13:27:05 +0200

    changes help-center-bar

--- step-definitions/help-center-bar.js
+++ step-definitions/help-center-bar.js
@@ -1,8 +1,36 @@
-let { $, sleep } = require('./funcs');
-const { username, password } = require('./credentials.json');
+let {
+  $,
+  sleep
+} = require('./funcs');
+const {
+  username,
+  password
+} = require('./credentials.json');
 
 module.exports = function () {
 
+
+
+  this.Given(/^that i have clicked on the menu and chosen Help center$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+
+
+  });
+
+
+
+  this.When(/^I type ratings in the help center bar$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+
+
+  this.Then(/^I should get related topics concerning ratings$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
 
 
 
