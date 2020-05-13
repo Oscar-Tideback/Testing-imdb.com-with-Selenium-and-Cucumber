1b858de0de7cfff1238bfc4e568d6b1346c94b4e
FireRay <jonashoglund282@gmail.com>
2020-05-13 16:51:05 +0200

    changes in help-center-bar

--- step-definitions/help-center-bar.js
+++ step-definitions/help-center-bar.js
@@ -11,7 +11,8 @@ const {
 module.exports = function () {
 
   let searchFieldHelp;
-  //let results = [];
+  let results = [];
+  let counter = 0;
 
   this.Given(/^that i have clicked on the menu and chosen Help center$/, async function () {
     // Write code here that turns the phrase above into concrete actions
@@ -22,7 +23,6 @@ module.exports = function () {
 
   });
 
-
   this.When(/^I type "([^"]*)" in the help center bar \+ ENTER$/, async function (search) {
     await searchFieldHelp.sendKeys(search)
     await searchFieldHelp.sendKeys(selenium.Key.ENTER);
@@ -30,29 +30,21 @@ module.exports = function () {
     //See test below :)
   });
 
-
-
-  this.Then(/^I should get related topics concerning the word "([^"]*)"$/, async function (searchString) {
+  this.Then(/^I should get related topics concerning "([^"]*)"$/, async function (searchString) {
     // Write code here that turns the phrase above into concrete actions
-    let results = await driver.findElement(By.class('.a-link-normal'))
+    await driver.wait(until.elementLocated(By.css('a.a-link-normal > h3')));
+    results = await $('a.a-link-normal > h3');
+    //for loop that check every instance of the h3 element contins 'ratings'.
+    for (let result of results) {
+      let text = await result.getText()
+      console.log(text);
 
-    let string = results.getText();
-    console.log(string);
-    /*let found = false
-      for (let result of results) {
-        console.log('text', await result.getText())
-        let text = await result.getText()
-        if (text.toLowerCase().includes(searchString.toLowerCase())) {
-          found = true
-          break
-        }
-    }*/
+      if (text.toLowerCase().includes(searchString.toLowerCase())) {
+        assert.include(text.toLowerCase(), searchString.toLowerCase(), 'search result contains search value');
 
-    //assert(found, 'expected to find ' + searchString)
-
-
+      }
+    }
 
   });
-
 
 }
