dafa12b63f7612d642f9c9f8397de891669b7b9f
jaztothemin <jazivarsak@gmail.com>
2020-05-13 19:49:35 +0200

    step def-celebrityBirthday

---
+++ step-definitions/search-celebrity-birthday.js
@@ -0,0 +1,66 @@
+let { $, sleep } = require('./funcs');
+const { username, password } = require('./credentials.json');
+module.exports = function () {
+
+
+  this.Given(/^that I am at the Birth Month Day of August (\d+)th$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+
+  this.Then(/^I should be taken to the Janet McTeer page when I click on it\.$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+
+  this.Given(/^that I am at the Birth Month Day of August (\d+)th$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Then(/^I should be taken to the Mark Strong page when I click on it\.$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+  this.Given(/^that I am at the Birth Month Day of August (\d+)th$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+  this.Then(/^I should be taken to the Jesse Williams page when I click on it\.$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+  this.Given(/^that I am at the Birth Month Day of August (\d+)th$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+  this.Then(/^I should be taken to the Stephanie Szostak page when I click on it\.$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+  this.Given(/^that I am at the Birth Month Day of August (\d+)th$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+  this.Then(/^I should be taken to the James Gunn page when I click on it\.$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+}
