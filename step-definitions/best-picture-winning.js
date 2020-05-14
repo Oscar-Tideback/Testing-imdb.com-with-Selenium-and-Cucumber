6e0c5b8ab54ceb691efd2886b11c90a431d6dc01
FireRay <jonashoglund282@gmail.com>
2020-05-14 21:18:05 +0200

    changes to best-picture-winning

--- step-definitions/best-picture-winning.js
+++ step-definitions/best-picture-winning.js
@@ -7,23 +7,49 @@ const {
   password
 } = require('./credentials.json');
 
-let results, winners;
-let winnerList = [];
+let clickRated, results;
+let linkList, winnerList = [];
 
 module.exports = function () {
 
   this.Given(/^that I am on the Best Picture\-winning site$/, async function () {
     //step 1: call the page
     await helpers.loadPage('https://www.imdb.com/search/title/?count=100&groups=oscar_best_picture_winners&sort=year%2Cdesc&ref_=nv_ch_osc');
-    await sleep(3000);
+    await sleep(2000);
+
   });
 
+  this.Given(/^I have clicked User Rating$/, async function () {
+
+    //step 2: find the class with all the links (Popularity, A-Z, User Rating etc.)
+    linkList = await $('.sorting a');
+    //step 3: Sort the class to get the "right" link to User Ratings 
+    for (let prop of linkList) {
+
+      let string = await prop.getText();
+      if (string == 'User Rating') {
+        clickRated = await prop;
+      }
+
+    }
+    //step 4: click on the User Ratings link
+    await clickRated.click();
+    await sleep(2000);
+
+  });
+
+
+  /* Scenario: Top ratedd picture winners
+   Given that I am on the Best Picture winning site ------ klart!
+   And I have clicked User Rating  ---- metod för att klicka på User Rating
+   Then I should see Best Picture - winner and their rating. ----- kolla att varje film kontext innehåller ratings*/
+
+
   this.Then(/^I should see Best Picture\-winner and their rating\.$/, async function () {
-    //step 2: check that we've found the results in a list on the page
+    //step 5: check that we've found the results in a list on the page
     results = await $('.lister.list.detail.sub-list');
     assert(results, 'Could not find any results');
-    //step 3: 
-    winnerList = await $('.lister-item-header a');
+    //step 6: 
 
     for (let prop of winnerList) {
 
