ee5329534665a3e9d0f82286453a76346c3ce9bf
jaztothemin <jazivarsak@gmail.com>
2020-05-14 17:20:15 +0200

    MORE CLEANING IN FILES

--- step-definitions/best-picture-winning.js
+++ step-definitions/best-picture-winning.js
@@ -1,12 +1,5 @@
-let {
-  $,
-  sleep
-} = require('./funcs');
-
-const {
-  username,
-  password
-} = require('./credentials.json');
+let { $, sleep } = require('./funcs');
+const { username, password } = require('./credentials.json');
 
 let results;
 let winnerList = [];
@@ -14,21 +7,14 @@ let winnerList = [];
 module.exports = function () {
 
   this.Given(/^that I am on the Best Picture\-winning site$/, async function () {
-
     await helpers.loadPage('https://www.imdb.com/search/title/?count=100&groups=oscar_best_picture_winners&sort=year%2Cdesc&ref_=nv_ch_osc');
     await sleep(2000);
-
-
   });
 
-
   this.Then(/^I should see Best Picture\-winner and their rating\.$/, async function () {
-
     //check that we've found the results in a list
     results = await $('.lister.list.detail.sub-list');
     assert(results, 'Could not find any results');
-
-
     let winner = await $('#text');
 
     console.log(winner);
@@ -39,12 +25,6 @@ module.exports = function () {
 
         console.log(prop.getText());
     }*/
-
-
-
   });
-
-
-
 
 }
