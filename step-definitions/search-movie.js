90b84fea7e0278addce2eebd7bb07d020258bb43
jaztothemin <jazivarsak@gmail.com>
2020-05-15 16:47:07 +0200

    some cleaning (error messages) in searchMovie

--- step-definitions/search-movie.js
+++ step-definitions/search-movie.js
@@ -19,19 +19,25 @@ module.exports = function () {
 
   this.When(/^I click on the search button$/, async function () {
     let searchButton = await $('#suggestion-search-button');
-    assert(searchButton, 'Could not find the search button');
+    assert(searchButton,
+      'Could not find the search button'
+    );
     await searchButton.click();
   });
 
   this.Then(/^the first search result should contain the word "([^"]*)"$/, async function (phrase) {
     await driver.wait(until.elementLocated(By.css('.findResult, .findNoResults')));
     let results = await $('.findResult');
-    assert(results, 'Could not find any results');
+    assert(results,
+      'Could not find any results'
+    );
     let firstResult = results[0];
     let resultText = await firstResult.getText();
     //console.log('resultText', resultText)
     //console.log('phrase', phrase);
-    assert.include(resultText, phrase, 'Could not find the phrase ' + phrase + ' in the first search result.');
+    assert.include(resultText, phrase,
+      'Could not find the phrase ' + phrase + ' in the first search result.'
+    );
     //await sleep(sleepTime);
   });
 
