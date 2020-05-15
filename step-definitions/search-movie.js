1a5b3d52142509303bc0b8f50e10ae505f7cda45
LadyQuinn <kiaa887@hotmail.com>
2020-05-15 17:00:29 +0200

    edited test code

--- step-definitions/search-movie.js
+++ step-definitions/search-movie.js
@@ -17,17 +17,6 @@ module.exports = function () {
     //await sleep(sleepTime);
   });
 
-<<<<<<< HEAD
-=======
-  this.When(/^I click on the search button$/, async function () {
-    let searchButton = await $('#suggestion-search-button');
-    assert(searchButton,
-      'Could not find the search button'
-    );
-    await searchButton.click();
-  });
-
->>>>>>> 06361b82f96949881886cf5576ba82e81eae6eea
   this.Then(/^the first search result should contain the word "([^"]*)"$/, async function (phrase) {
     await driver.wait(until.elementLocated(By.css('.findResult, .findNoResults')));
     let results = await $('.findResult');
@@ -36,15 +25,7 @@ module.exports = function () {
     );
     let firstResult = results[0];
     let resultText = await firstResult.getText();
-<<<<<<< HEAD
     assert.include(resultText, phrase, 'Could not find the phrase ' + phrase + ' in the first search result.');
-=======
-    //console.log('resultText', resultText)
-    //console.log('phrase', phrase);
-    assert.include(resultText, phrase,
-      'Could not find the phrase ' + phrase + ' in the first search result.'
-    );
->>>>>>> 06361b82f96949881886cf5576ba82e81eae6eea
     //await sleep(sleepTime);
   });
 }
