06361b82f96949881886cf5576ba82e81eae6eea
jaztothemin <jazivarsak@gmail.com>
2020-05-15 16:49:24 +0200

    changes / cleaning castCrew

--- step-definitions/cast-crew.js
+++ step-definitions/cast-crew.js
@@ -5,21 +5,23 @@ module.exports = function () {
 
   this.Given(/^that I am on the Suits page$/, async function () {
     await helpers.loadPage('https://www.imdb.com/title/tt1632701/?ref_=nv_sr_srsg_0');
-    await sleep(1000);
+    //await sleep(1000);
   });
 
   this.Given(/^that I have clicked the Edit button$/, async function () {
     let editButton = await $('.btn.primary.large');
-    //assert(searchButton, 'Could not find the search button');
+    //assert(searchButton, 
+    // 'Could not find the search button'
+    // );
     await editButton.click();
-    await sleep(4000);
+    //await sleep(4000);
   });
 
   this.Given(/^that I haved chosen wanted Cast in Cast&Crew and clicked on the continue button$/, async function () {
     let editButton = await $('.btn.primary');
     //assert(searchButton, 'Could not find the search button');
     await editButton.click();
-    await sleep(4000);
+    //await sleep(4000);
   });
 
   /*this.When(/^I add data in Gabriel Macht,Episode Errors and Omissions \(\#(\d+)\.(\d+)\),Harvey Specter and click button check these updates$/, function (arg1, arg2, callback) {
