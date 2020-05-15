d158de28282c355f553f0b61c44dff7f8ebfaad2
void00 <tideback@gmail.com>
2020-05-15 16:33:40 +0200

    mer bug-fix watchlist

--- step-definitions/add-to-watchlist.js
+++ step-definitions/add-to-watchlist.js
@@ -6,7 +6,7 @@ module.exports = function () {
   let movieName;
 
   //Scenario: When I have signed in, I would like to add tom my watchlist
-  this.When(/^I have signed in to the page$/, async function () {
+  this.Given(/^There are no movies in my watchlist and I have signed in to the page$/, async function () {
     await helpers.loadPage('https://www.imdb.com/registration/signin?ref=nv_generic_lgin&u=%2F');
     await driver.findElement(By.linkText("Sign in with IMDb")).click();
     await driver.findElement(By.id('ap_email')).sendKeys(username);
@@ -17,8 +17,9 @@ module.exports = function () {
   this.Then(/^click add to watchlist on the first move on page$/, async function () {
     driver.executeScript("window.scrollBy(0,1000)", "");
     await driver.wait(until.elementLocated(By.css('.fan-picks')), 10000);
-    await driver.wait(until.elementLocated(By.css('.ipc-poster-card .ipc-watchlist-ribbon')), 10000);
-    await driver.findElement(By.css('.ipc-poster-card .ipc-watchlist-ribbon[aria-label="add to watchlist"]')).click();
+    await driver.wait(until.elementLocated(By.css('.ipc-poster-card .ipc-watchlist-ribbon[aria-label="add to watchlist"]')), 10000);
+    let toClick = await driver.findElement(By.css('.ipc-poster-card .ipc-watchlist-ribbon[aria-label="add to watchlist"]'));
+    await toClick.click();
     await driver.wait(until.elementLocated(By.css('.ipc-poster-card .ipc-watchlist-ribbon[aria-label="remove from watchlist"]')), 10000);
   });
 
