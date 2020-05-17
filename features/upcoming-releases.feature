6aa2b0fd6143908f3830950e5638df926d71d993
LadyQuinn <kiaa887@hotmail.com>
2020-05-17 20:04:56 +0200

    added test code for upcoming releases

---
+++ features/upcoming-releases.feature
@@ -0,0 +1,7 @@
+Feature: As an user I want to see a list of upcoming releases for Austria so that I can see what new movies and shows are being released
+
+  Scenario: View upcoming releases for Austria
+    Given that I am on the IMDB site
+    And I have clicked on the Menu button
+    When I have clicked on Release Calender
+    Then I should see a list of upcoming releases for Austria by clicking on Austria
---
+++ step-definitions/upcoming-releases.js
@@ -0,0 +1,18 @@
+let { $, sleep } = require('./funcs');
+
+module.exports = function () {
+
+  let sleepTime = 5000;
+
+  this.When(/^I have clicked on Release Calender$/, async function () {
+    let linkCalendar = await driver.findElement(By.css("a[href*='calendar']"));
+    linkCalendar.click();
+    await sleep(sleepTime);
+  });
+
+  this.Then(/^I should see a list of upcoming releases for Austria by clicking on Austria$/, async function () {
+    let linkReleases = await driver.findElement(By.css("a[href*='AT&ref']"));
+    linkReleases.click();
+    await sleep(sleepTime);
+  });
+}
