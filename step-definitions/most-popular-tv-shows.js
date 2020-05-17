83121b76dcf6f4757990ce62f009de2ff1e3665a
LadyQuinn <kiaa887@hotmail.com>
2020-05-17 20:10:48 +0200

    edited test codes

--- step-definitions/most-popular-tv-shows.js
+++ step-definitions/most-popular-tv-shows.js
@@ -1,22 +1,23 @@
 let { $, sleep } = require('./funcs');
+const { username, password } = require('./credentials.json');
 
 module.exports = function () {
 
-  let sleepTime = 1000;
+  //let sleepTime = 1000;
 
   this.Given(/^that I am on the IMDB site\.$/, async function () {
     await helpers.loadPage('https://www.imdb.com');
-    await sleep(sleepTime);
+    //await sleep(sleepTime);
   });
 
   this.When(/^I click on the Menu button$/, async function () {
     let menuButton = await driver.findElement(By.css('.ipc-button')).click();
-    await sleep(sleepTime);
+    //await sleep(sleepTime);
   });
 
   this.Then(/^I should see a list of popular shows by clicking on the link Most Popular Shows$/, async function () {
     let link4 = await driver.findElement(By.css("a[href*='tvmeter']"));
     link4.click();
-    await sleep(sleepTime);
+    //await sleep(sleepTime);
   });
 }
--- step-definitions/search-movie.js
+++ step-definitions/search-movie.js
@@ -1,4 +1,5 @@
 let { $, sleep } = require('./funcs');
+const { username, password } = require('./credentials.json');
 
 module.exports = function () {
 
