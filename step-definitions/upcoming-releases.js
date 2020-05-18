a719dca26a7eeabf43b39702a6c25e8f6e91f7fc
LadyQuinn <kiaa887@hotmail.com>
2020-05-18 17:25:22 +0200

    edited test code for upcomin releases

--- step-definitions/upcoming-releases.js
+++ step-definitions/upcoming-releases.js
@@ -3,17 +3,23 @@ const { username, password } = require('./credentials.json');
 
 module.exports = function () {
 
-  let sleepTime = 5000;
+  //let sleepTime = 5000;
 
-  this.When(/^I have clicked on Release Calender$/, async function () {
+  this.When(/^I have clicked on Release Calendar$/, async function () {
     let linkCalendar = await driver.findElement(By.css("a[href*='calendar']"));
     linkCalendar.click();
-    await sleep(sleepTime);
+    //await sleep(sleepTime);
   });
 
   this.Then(/^I should see a list of upcoming releases for Austria by clicking on Austria$/, async function () {
     let linkReleases = await driver.findElement(By.css("a[href*='AT&ref']"));
     linkReleases.click();
-    await sleep(sleepTime);
+    let title = await $('title');
+    let titleText = await title.getAttribute("textContent");
+    // expect the  title text to include the actors name
+    expect(titleText,
+      'Upcoming Releases for Austria could not be found'
+    ).to.include('Upcoming Releases for Austria')
+    //await sleep(sleepTime);
   });
 }
