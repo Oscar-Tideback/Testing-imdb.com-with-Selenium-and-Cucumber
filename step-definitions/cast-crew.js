c67ecd188e0a4c0ecfdaca04d574164da97e0796
FireRay <jonashoglund282@gmail.com>
2020-05-18 10:02:29 +0200

    changes

--- step-definitions/cast-crew.js
+++ step-definitions/cast-crew.js
@@ -1,27 +1,46 @@
 let { $, sleep } = require('./funcs');
 const { username, password } = require('./credentials.json');
 
+
+
 module.exports = function () {
 
   this.Given(/^that I am on the Suits page$/, async function () {
+    //step 1: log in as a user
+    await helpers.loadPage('https://www.imdb.com/registration/signin?ref=nv_generic_lgin&u=%2F');
+    await driver.findElement(By.linkText("Sign in with IMDb")).click();
+    await driver.findElement(By.id('ap_email')).sendKeys(username);
+    await driver.findElement(By.id('ap_password')).sendKeys(password);
+    await driver.findElement(By.id('signInSubmit')).click();
+    //step 2: go to the Suits page
     await helpers.loadPage('https://www.imdb.com/title/tt1632701/?ref_=nv_sr_srsg_0');
-    //await sleep(1000);
+    await sleep(2000);
   });
 
   this.Given(/^that I have clicked the Edit button$/, async function () {
+
+    //step 3: click the edit button on the Suits page
     let editButton = await $('.btn.primary.large');
-    //assert(searchButton, 
-    // 'Could not find the search button'
-    // );
+    assert(editButton,
+      'Could not find the search button'
+    );
     await editButton.click();
-    //await sleep(4000);
+    //await sleep(2000);
+
+    //step 3: select categories
+
+    let chooseButton = await selectOption('select.choose', '2.');
+    await chooseButton.click();
+    await sleep(2000);
+
+
   });
 
   this.Given(/^that I haved chosen wanted Cast in Cast&Crew and clicked on the continue button$/, async function () {
     let editButton = await $('.btn.primary');
     //assert(searchButton, 'Could not find the search button');
     await editButton.click();
-    //await sleep(4000);
+    await sleep(2000);
   });
 
   /*this.When(/^I add data in Gabriel Macht,Episode Errors and Omissions \(\#(\d+)\.(\d+)\),Harvey Specter and click button check these updates$/, function (arg1, arg2, callback) {
--- step-definitions/funcs.js
+++ step-definitions/funcs.js
@@ -12,5 +12,24 @@ module.exports = {
   sleep: function (ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
+
+
+}
+
+global.selectOption = async (cssSelector, optionName) => {
+  let codeToRun = `(()=>{
+      let select = document.querySelector('cssSelector');
+      if(!select){return;}
+      let options = select.querySelectorAll('option');
+      options.forEach((option,i) => {
+        if(option.innerText === 'optionName'){
+          select.selectedIndex = i;
+          select.dispatchEvent(new Event('change', { bubbles: true }));
+        }
+      })
+    })()`
+    .split('cssSelector').join(cssSelector)
+    .split('optionName').join(optionName);
+  await driver.executeScript(codeToRun);
 }
 
