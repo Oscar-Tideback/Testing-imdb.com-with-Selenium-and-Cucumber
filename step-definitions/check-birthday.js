aed7bc4f62ed9d37398c98e28f5b56ab09ff2f8d
void00 <tideback@gmail.com>
2020-05-18 14:21:08 +0200

    footer

--- step-definitions/check-birthday.js
+++ step-definitions/check-birthday.js
@@ -1,13 +1,10 @@
-let {
-  $,
-  sleep
-} = require('./funcs');
+let { $, sleep } = require('./funcs');
 
 module.exports = function () {
   let names = [];
   let wikipediaCeleb = [];
   let wikiBday = [];
-  let name, rightDate, ourDate, numberOfCelebMax50;
+  let rightDate, ourDate, numberOfCelebMax50;
 
   //Scenario: Cross-check our date of birth with the celebertys wikipedia pages
   this.When(/^I browse to Birth Month Day of todays$/, async function () {
@@ -27,7 +24,7 @@ module.exports = function () {
   this.Then(/^if the list contains white space replace that with _$/, async function () {
     names.slice(0, numberOfCelebMax50); //Reduce the list of names, just cleaning the list
     for (let i = 0; i < numberOfCelebMax50; i++) {
-      name = await names[i].getText();
+      let name = await names[i].getText();
       let str = name.replace(" ", "_");//Finding " " between forename and lastname and replaces it with "_"
       names[i] = str;
     }
@@ -44,19 +41,17 @@ module.exports = function () {
   });
 
   this.Then(/^find birthday on that page and check if it is today$/, async function () {
-    let today = new Date();
+    let today = new Date();//BUG, Fix is. check what date IMDb is on right now
     ourDate = today.getMonth() + 1;
     ourDate += "-";
-    ourDate += today.getDate();// Made string with todays date in format 01-13
+    ourDate += today.getDate();// Made string with todays date with format 01-13
 
     for (let i = 0; i < numberOfCelebMax50; i++) {
       await helpers.loadPage(wikipediaCeleb[i]);
       wikiBday[i] = await $(".bday");
       await driver.wait(until.elementLocated(By.id('footer-copyrightico')), 10000);
       if ((wikiBday[i]) !== null) {//Check if wiki is missing birthday
-        console.log(i);
         let str = await wikiBday[i].getAttribute("textContent");
-        console.log(str);
         let str2 = str.slice(5, str.length);
         if (today.getMonth() < 9) //This might be wrong, should it be 10?// Removes a 0 if month is 01-09 so 1-9
           rightDate = str2.slice(1, str2.length);
--- step-definitions/check-footer.js
+++ step-definitions/check-footer.js
@@ -1,25 +1,55 @@
 let { $, sleep } = require('./funcs');
 
 module.exports = function () {
-
+  let mainWin;
   this.When(/^I browse the statpage$/, async function () {
     await helpers.loadPage('https://www.imdb.com/');
+
   });
 
   this.Then(/^the footer should contain the information$/, async function (table) {
-    //List < List < String >> data = table.raw();
     const data = table.raw();
-    console.log(data[1][1], ' OCH ', data[1][1][1]);
-
+    let linkText = await $('.ipc-inline-list__item');
+    let i = 0;
+    for (let result of linkText) {
+      let text = await result.getText();
+      if (text) {
+        i++;
+        expect(text,
+          'The link text did not match the table text '
+        ).to.equal(data[i][0]);
+      }
+    }
   });
 
-  this.Then(/^images and extrenal links to$/, function (table) {
+  this.Then(/^images and extrenal links to$/, async function (table) {
+    const data = table.raw();
+    let linkText = await $('._27Hr-itnXHPpOoaB59daSz ul._1W6LtHiV5M-NUnes6xN1az li');
+    let i = 0;
+    for (let result of linkText) {
+      let linkText = await $('.ipc-inline-list__item [aria-label="' + data[i + 1][0] + '"]');
+      let text = await linkText.getAttribute("title");
+      i++;
+      expect(text,
+        'The link icon did not match the title text '
+      ).to.equal(data[i][0]);
+    }
   });
 
-  this.Then(/^the text "([^"]*)"$/, function (copyRightText) {
+  this.Then(/^the text "([^"]*)"$/, async function (copyRightText) {
+    let copyRight = await $('p.imdb-footer__copyright._1qNTRY72M5RnFTeiT5Ho-6');
+    copyRight = await copyRight.getAttribute("innerText");
+
+    expect(copyRight,
+      'The copyright text could not be found'
+    ).to.equal(copyRightText);
   });
 
-  this.Then(/^that IMDb loggo is pressent$/, function () {
+  this.Then(/^that IMDb loggo is pressent$/, async function () {
+    let logo = await $('.imdb-footer__logo');
+    expect(logo,
+      'The footer logo could not be found'
+    ).to.exist;
   });
 
 }
